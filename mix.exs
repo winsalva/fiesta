1e9c819b4e0d7b95bdf8d152cc626be76dcd0668
Darren Gegantino <darren.gegantino@martide.com>
2020-09-26 01:24:48 +0800

    Add apps to plt

--- mix.exs
+++ mix.exs
@@ -13,7 +13,8 @@ defmodule Kusina.MixProject do
       deps: deps(),
       dialyzer: [
         plt_core_path: "priv/plts",
-        plt_file: {:no_warn, "priv/plts/dialyzer.plt"}
+        plt_file: {:no_warn, "priv/plts/dialyzer.plt"},
+        plt_add_apps: [:mix, :distillery, :ex_unit]
       ]
     ]
   end
